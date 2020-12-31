168e6e74940961dd98096e4b4530aaf31436cd1a
ArisatoX <patricksungkharisma27@gmail.com>
2020-12-31 23:43:59 +0700

    respawn animal after killed

--- scripts/bulletCollision.js
+++ scripts/bulletCollision.js
@@ -13,12 +13,20 @@ const bulletCollided = event => {
     }
     else if (event.detail.body.el.className === 'target') 
     {
-        console.log("hit animal");
         event.detail.target.el.removeEventListener('collide', bulletCollided);
 
-        // Remove Animal (temp)
+        // Remove animal (temp)
         mainScene.removeChild(event.detail.body.el);
         scoreIncrement();
+
+        // Add new animal
+        const animal = document.createElement("a-entity");
+        animal.setAttribute("class", "target");
+        animal.setAttribute("dynamic-body", "linearDamping: 0.9; angularDamping: 0.9;");
+        animal.setAttribute("random-position", "min: -25 0 -15; max: 20 0 25;");
+        animal.setAttribute("scale", "0.3 0.3 0.3");
+        animal.setAttribute("gltf-model", "#horse");
+        mainScene.appendChild(animal);
     }
 };
 
